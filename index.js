b1cb709fd5b1619cf0590317fc28d5b3e99c21ec
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-10-29 22:30:10 +0900

    fix: fix an error while compiling

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ module.exports = function (content, map, meta) {
   return content
     .replace(/(?<pre>[^;\s\n}])(?<aft>[\s\n]*})/g, "$<pre>;$<aft>")
     .replace(/{.+?}/sg, container => {
-    const props = container.substr(1).slice(0, -1);
-    return "{" + props.replace(/[^;:{}]+?:[^;:{}]+?;/sg, prop => {
-      if(prop.indexOf("vh") < 0){
-        return prop;
-      }else{
-        return prop + prop.replace(/-?[\d\.]+vh/g, part => {
-          return "calc(var(--vh,1vh)*" + part.match(/(-?[\d\.]+)/)[1] + ")";
-        })
-      }
-    }) + "}";
-  });
+      const props = container.substr(1).slice(0, -1);
+      return "{" + props.replace(/[^;:{}]+?:[^;:{}]+?;/sg, prop => {
+        if(prop.indexOf("vh") < 0){
+          return prop;
+        }else{
+          return prop + prop.replace(/-?[\d\.]+vh/g, part => {
+            return "calc(var(--vh,1vh)*" + part.match(/(-?[\d\.]+)/)[1] + ")";
+          })
+        }
+      }) + "}";
+    })
+    .replace(/;}/g, "}");
 }
